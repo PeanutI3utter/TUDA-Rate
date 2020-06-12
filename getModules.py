4b773e4ab1c2382b37e0aa4cffe3c6f56c137d18
PeanutI3utter <sijunkwon@gmail.com>
2020-06-12 18:52:19 +0200

    division of winter and sommer semester

--- getModules.py
+++ getModules.py
@@ -1,27 +1,54 @@
 from pyquery import PyQuery as pq
 
-class Course:
-    def __init__(self, name, link):
+class Category:
+    def __init__(self, name, url):
         self.name = name
-        self.link = 'www.tucan.tu-darmstadt.de' + link
+        self.url = url
 
-    def getLink(self):
-        return self.link
+    def traverse(self):
+        subcategories = pq(url=self.url)('ul').filter('.auditRegistrationList')
+        if len(subcategories) > 0:
+            subcategories = subcategories.children()
+            
 
-    def getName(self):
-        return self.name
 
-    def __str__(self):
-        return '{\n\tCourse: ' + str(self.name) + '\n\tLink: ' + str(self.link) + '\n}'
+class CourseCatalog:
+    def __init__(self):
+        wise, sose = getLatestSemester()
+        self.subcategories = [wise, sose]
 
-    def __repr__(self):
-        return '{\n\tCourse: ' + str(self.name) + '\n\tLink: ' + str(self.link) + '\n}'
+    def buildCatalog(self):
+        [subcat.traverse() for subcat in self.subcategories]
+
+
+
 
     
-d = pq(url="https://www.tucan.tu-darmstadt.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=ACTION&ARGUMENTS=-ARykwIoAoe25QV72nKSmnPe~eIKBRCEh1ONpCunbzfygB8Slxcja9vHWtJjrgkT2M7meJQ5Hw~V~Ik1CiYMplOjb8zhzZdMDEYSL~IJ5mKiWxUFlx3tWh5gweWapt64HGfWAjElStoPfHj8ou2yypXcBe02tRfwGf5MC-X694JWcdogEXTb6yoHAk2A__")
-fblist = d('ul').filter('.auditRegistrationList').children()
+def getLatestSemester():
+    d = pq(url='https://www.tucan.tu-darmstadt.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=EXTERNALPAGES&ARGUMENTS=-N000000000000001,-N000463,-Avvarchivstart%2Ehtml')
+    pastsemester = d('li').filter(lambda i, this: pq(this).attr['class'] == "intern depth_3 linkItem ")
 
-courseRefs = [Course(course.attr.title, course('a').attr.href) for course in  fblist.items()]
+
+    tmp = pastsemester.eq(0)
+    tmp2 = pastsemester.eq(1)
+
+    text1 = tmp('a').text()
+
+    if 'Winter' in text1:
+        wise = tmp
+        sose = tmp2
+    else:
+        wise = tmp2
+        sose = tmp
+
+    wiseurl = "https://www.tucan.tu-darmstadt.de" + str(pq(wise)('a').attr.href)
+    soseurl = "https://www.tucan.tu-darmstadt.de" + str(pq(sose)('a').attr.href)
+
+    wise = Category("Wintersemester", wiseurl)
+    sose = Category("Sommersemester", soseurl)
+    return wise, sose
 
 
-print(courseRefs)
+cc = CourseCatalog()
+cc.buildCatalog()
+print(cc.subcategories)
